c03c0f62c053762430f1221bac29f12e313e3974
SerhiyDemchuk <sergeylobster@gmail.com>
2021-02-23 10:05:28 +0200

    com

--- components/Posts/Post.tsx
+++ components/Posts/Post.tsx
@@ -19,7 +19,13 @@ const useStyles = makeStyles(
         },
         cardContent: {
             maxWidth: 300,
-            height: '100%',
+            width: 300,
+        },
+        title: {
+            fontSize: 30
+        },
+        body: {
+
         },
         button: {
             background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
@@ -39,67 +45,66 @@ interface PostProps {
     body: string,
     id: number,
     postId: number,
-    delPost: (number) => void,
-    updPost: (number) => void,
-    addPost: (number) => void,
-    setNewText: (string) => void,
-    setNewTitle: (string) => void,
+    delPost(id: number): void,
+    updPost(id: number): void,
+    addPost(id: number): void,
+    setNewText(string: string): void,
+    setNewTitle(string: string): void,
 }
 
 const Post: React.FC<PostProps> = ({ title, body, id, postId, ...props }): React.ReactElement => {
     const dispatch = useDispatch();
     const classes = useStyles();
-    
+
     return (
         <div>
-            <Link href={{
-                pathname: "/posts/[id]",
-                query: { id: id }
-            }}>
-                <a>
-                    <Grid item>
-                        <Card className={classes.root}>
-                            <ButtonBase
-                                onClick={() => dispatch(retrievePost(id))}>
-                                <CardContent className={classes.cardContent}>
-                                    <div>
+            <Grid item>
+                <Card className={classes.root}>
+                    <ButtonBase
+                        onClick={() => dispatch(retrievePost(id))}>
+                        <Link href={{
+                            pathname: "/posts/[id]",
+                            query: { id: id }
+                        }}>
+                            <a>
+                                <CardContent onClick={() => dispatch(retrievePost(id))} className={classes.cardContent}>
+                                    <div className={classes.title}>
                                         <Typography variant="h5" component="h2">
                                             {title}
                                         </Typography>
                                     </div>
-                                    <div>
+                                    <div className={classes.body}>
                                         <Typography variant="body2" component="p">
                                             {body}
                                         </Typography>
                                     </div>
                                 </CardContent>
-                            </ButtonBase>
-                            <div>
-                                <Button className={classes.button} onClick={() => props.delPost(id)}>Delete</Button>
-                                <Button className={classes.button} onClick={() => props.updPost(id)}>Update</Button>
-                            </div>
-                        </Card>
-                        {postId === id ? (
-                            <Card>
-                                <CardContent>
-                                    <Typography variant="h6">
-                                        Type title
-                                        </Typography>
-                                    <Input onChange={(e: React.ChangeEvent<HTMLInputElement>) => props.setNewTitle(e.target.value)} placeholder="title" autoFocus />
-                                    <Typography variant="h6">
-                                        Type text
-                                        </Typography>
-                                    <Input onChange={(e: React.ChangeEvent<HTMLInputElement>) => props.setNewText(e.target.value)} placeholder="text" autoFocus />
-                                </CardContent>
-                                <Button variant="text" color="default" onClick={() => props.addPost(id)}>Add</Button>
-                            </Card>)
-                            : null
-                        }
-                    </Grid>
-                </a>
-            </Link>
+                            </a>
+                        </Link>
+                    </ButtonBase>
+                    <div>
+                        <Button className={classes.button} onClick={() => props.delPost(id)}>Delete</Button>
+                        <Button className={classes.button} onClick={() => props.updPost(id)}>Update</Button>
+                    </div>
+                </Card>
+                {postId === id ? (
+                    <Card>
+                        <CardContent>
+                            <Typography variant="h6">
+                                Type title
+                                    </Typography>
+                            <Input onChange={(e: React.ChangeEvent<HTMLInputElement>) => props.setNewTitle(e.target.value)} placeholder="title" autoFocus />
+                            <Typography variant="h6">
+                                Type text
+                                    </Typography>
+                            <Input onChange={(e: React.ChangeEvent<HTMLInputElement>) => props.setNewText(e.target.value)} placeholder="text" autoFocus />
+                        </CardContent>
+                        <Button variant="text" color="default" onClick={() => props.addPost(id)}>Add</Button>
+                    </Card>)
+                    : null}
+            </Grid>
         </div>
-    )
+    );
 }
 
 Post.propTypes = {
